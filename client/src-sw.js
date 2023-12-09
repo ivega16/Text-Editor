1cdd5b5bbdb6b9bc0e7f0149db779eb6910d9b75
Isa Vega <imvega100@gmail.com>
2023-12-08 21:12:39 -0500

    fixed src-sw

--- client/src-sw.js
+++ client/src-sw.js
@@ -37,7 +37,7 @@ registerRoute(
       plugins: [
         // This plugin will cache responses with these headers to a maximum-age of 30 days
         new CacheableResponsePlugin({
-          statuses: [0, 200],
+          statuses: [0, 200]
         }),
       ],
     })
